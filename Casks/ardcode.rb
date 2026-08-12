25bc6ad2d9c23513fb928bf299f38538cfcf294a
ardhi <ardhi@kertas.co.id>
2026-08-13 06:10:04 +0700

    Publish notarized ArdCode v0.2.1

--- Casks/ardcode.rb
+++ Casks/ardcode.rb
@@ -1,6 +1,6 @@
 cask "ardcode" do
-  version "0.2.0"
-  sha256 "1871464812763ff694d2c93b24a3d0d23591fa2851350efa5e1d1fa68517e54a"
+  version "0.2.1"
+  sha256 "3be6113cdd372ae9c6e97551f892a55d1b30a59991fee31790b3a1f52f29b171"
 
   url "https://github.com/ardvis/ardcode-dist/raw/refs/tags/v#{version}/releases/v#{version}/ardcode-macos-arm64.tar.gz"
   name "ArdCode"
