2f36eac68b58597db9e3c8d23a2aef7301345c27
ardhi <ardhi@kertas.co.id>
2026-08-20 01:08:52 +0700

    Declare Homebrew SQLite dependency

--- Casks/ardcode.rb
+++ Casks/ardcode.rb
@@ -7,6 +7,7 @@ cask "ardcode" do
     url "https://github.com/ardvis/ardcode-dist/releases/download/v#{version}/ardcode-macos-arm64.tar.gz"
     depends_on macos: :ventura
     depends_on arch: :arm64
+    depends_on formula: "sqlite"
     artifact "Ardcode_ArdcodeCLI.bundle",
              target: "#{HOMEBREW_PREFIX}/bin/Ardcode_ArdcodeCLI.bundle"
   end
@@ -14,6 +15,7 @@ cask "ardcode" do
   on_linux do
     url "https://github.com/ardvis/ardcode-dist/releases/download/v#{version}/ardcode-linux-x64.tar.gz"
     depends_on arch: :x86_64
+    depends_on formula: "sqlite"
     depends_on formula: "libsecret"
     artifact "Ardcode_ArdcodeCLI.resources",
              target: "#{HOMEBREW_PREFIX}/bin/Ardcode_ArdcodeCLI.resources"
