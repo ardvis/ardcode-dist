45bd0db81a095a39cde6804bd2c8f944dc57a5ad
ardhi <ardhi@kertas.co.id>
2026-08-19 19:44:17 +0700

    Install platform dashboard resources

--- Casks/ardcode.rb
+++ Casks/ardcode.rb
@@ -7,12 +7,16 @@ cask "ardcode" do
     url "https://github.com/ardvis/ardcode-dist/releases/download/v#{version}/ardcode-macos-arm64.tar.gz"
     depends_on macos: :ventura
     depends_on arch: :arm64
+    artifact "Ardcode_ArdcodeCLI.bundle",
+             target: "#{HOMEBREW_PREFIX}/bin/Ardcode_ArdcodeCLI.bundle"
   end
 
   on_linux do
     url "https://github.com/ardvis/ardcode-dist/releases/download/v#{version}/ardcode-linux-x64.tar.gz"
     depends_on arch: :x86_64
     depends_on formula: "libsecret"
+    artifact "Ardcode_ArdcodeCLI.resources",
+             target: "#{HOMEBREW_PREFIX}/bin/Ardcode_ArdcodeCLI.resources"
   end
 
   name "Ardcode"
@@ -20,9 +24,6 @@ cask "ardcode" do
   homepage "https://github.com/ardvis/ardcode-dist"
 
   binary "ardcode"
-  artifact "Ardcode_ArdcodeCLI.bundle",
-           target: "#{HOMEBREW_PREFIX}/bin/Ardcode_ArdcodeCLI.bundle"
-
   postflight do
     system_command "#{staged_path}/ardcode",
                    args: ["install-global", "--managed-only"]
