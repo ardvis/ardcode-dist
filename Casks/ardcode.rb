da90a7e598c986242a88a5aceae116620d340b25
ardhi <ardhi@kertas.co.id>
2026-08-20 16:23:10 +0700

    Publish Ardcode v0.4.9 release metadata

--- Casks/ardcode.rb
+++ Casks/ardcode.rb
@@ -1,7 +1,7 @@
 cask "ardcode" do
-  version "0.4.8"
-  sha256 arm:          "0f3c2d90c3e3fd18233fe7fd8866d65c9888597a0e6bb27a4e80eb4ba0526ac0",
-         x86_64_linux: "1e7a894f93c2a9ee70527ae99744e18d76cc6377f99150940c4bd3a0afa2883d"
+  version "0.4.9"
+  sha256 arm:          "435062f2d492842077602affa2a07426d7f6c7944a4c550e5d64f598c1501d86",
+         x86_64_linux: "ac78db1472a08139987920e0ecb09a5ecd223b0538b4d4e5de89bbfef002107b"
 
   on_macos do
     url "https://github.com/ardvis/ardcode-dist/releases/download/v#{version}/ardcode-macos-arm64.tar.gz"
