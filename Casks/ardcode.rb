99b3e12465fe7b877f366ed8ca0c056d8980c423
ardhi <ardhi@kertas.co.id>
2026-08-25 00:28:41 +0700

    Declare llama.cpp runtime dependency

--- Casks/ardcode.rb
+++ Casks/ardcode.rb
@@ -8,6 +8,7 @@ cask "ardcode" do
     depends_on macos: :ventura
     depends_on arch: :arm64
     depends_on formula: "sqlite"
+    depends_on formula: "llama.cpp"
     artifact "Ardcode_ArdcodeCLI.bundle",
              target: "#{HOMEBREW_PREFIX}/bin/Ardcode_ArdcodeCLI.bundle"
   end
@@ -16,6 +17,7 @@ cask "ardcode" do
     url "https://github.com/ardvis/ardcode-dist/releases/download/v#{version}/ardcode-linux-x64.tar.gz"
     depends_on arch: :x86_64
     depends_on formula: "sqlite"
+    depends_on formula: "llama.cpp"
     depends_on formula: "libsecret"
     artifact "Ardcode_ArdcodeCLI.resources",
              target: "#{HOMEBREW_PREFIX}/bin/Ardcode_ArdcodeCLI.resources"
